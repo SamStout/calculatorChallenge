2ae02fabe64340f4adfee5464e17efb56a5a70ba
samiistout <samuelstout6580@gmail.com>
2022-07-27 15:35:10 -0500

    defined variables in JS

---
+++ script.js
@@ -0,0 +1,5 @@
+const symbols = document.querySelector("#symbols")
+const firstValue = document.querySelector("#firstValue")
+const secondValue = document.querySelector("#secondValue")
+const form = document.querySelector("form");
+
