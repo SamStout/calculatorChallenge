68580cc1f953fc315d3fa009b3b07759f411b546
samiistout <samuelstout6580@gmail.com>
2022-07-27 16:46:54 -0500

    redid everything on html, starting from scratch

---
+++ scratch.js
@@ -0,0 +1,14 @@
+stringArr = [firstValue, symbol ,secondValue]
+
+const calculateResult =() => {
+    if (symbol.value="subtract") {
+     const finalResult = (Number(firstValue.value))-(Number(secondValue.value))
+     alert (finalResult)
+    }
+}
+
+
+
+
+form.addEventListener()
+form.addEventListener("submit", calculateResult); 
--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-const symbols = document.querySelector("#symbols")
+const symbols = document.querySelector("#symbol")
 const firstValue = document.querySelector("#firstValue")
 const secondValue = document.querySelector("#secondValue")
 const form = document.querySelector("form");
 
 
-stringArr = [firstValue,symbol,secondValue]
 
-const calculateResult =() => {
-    if symbol="subtract" {
-     const finalResult = (Number(firstValue))-(Number(secondValue))
-     alert (finalResult)
-    }
-}
-
-
-
-
-form.addEventListener()
-form.addEventListener("submit", calculateResult); 
