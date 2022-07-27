5590f4b410e3e1a3dc5ada8d19e1dfea4ea1c97d
samiistout <samuelstout6580@gmail.com>
2022-07-27 16:08:01 -0500

    Added function for calculating result

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const firstValue = document.querySelector("#firstValue")
 const secondValue = document.querySelector("#secondValue")
 const form = document.querySelector("form");
 
+
+stringArr = [firstValue,symbol,secondValue]
+
+const calculateResult =() => {
+    if symbol="subtract" {
+     const finalResult = (Number(firstValue))-(Number(secondValue))
+     alert (finalResult)
+    }
+}
+
+
+
+
+form.addEventListener()
+form.addEventListener("submit", calculateResult); 
