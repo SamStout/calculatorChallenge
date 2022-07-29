b86f13f62b3eb9c42c1dd393f8e592ea811604e0
samiistout <samuelstout6580@gmail.com>
2022-07-29 16:37:36 -0500

    changed input to div instead

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-const symbols = document.querySelector("#symbol")
-const firstValue = document.querySelector("#firstValue")
-const secondValue = document.querySelector("#secondValue")
-const form = document.querySelector("form");
+const multiply = document.querySelector("#multiply");
+const minus = document.querySelector("#minus");
+const plus = document.querySelector("#plus");
+const divide = document.querySelector("#divide");
+const one= document.querySelector("#one");
+const two= document.querySelector("#two");
+const three= document.querySelector("#three");
+const four= document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+const zero = document.querySelector("#zero");
+const equals = document.querySelector("#equals");
+const clear = document.querySelector("#clear");
+const decimals =document.querySelector("#decimals");
+const symbol = document.querySelector(".symbol");
+const button = document.querySelector("button");
+const display = document.querySelector("#display");
+const input = document.querySelector("input")
 
+const displayArr = [0]
+input.innerHTML = displayNumber
+const displayNumber = displayArr[i]
+//// 2 arrays put value of input into array as strings
+// operator button logs and contacts and converts array values into integer
 
+const inputValue = () => {display.innerHTML = displayNumber;{
+ displayNumber = displayArr.push(button.value)
+}
+}
 
+button.addEventListener(click,inputValue);
